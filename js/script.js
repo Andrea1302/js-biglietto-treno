9bf9ff0cc94c307a99935ca12c159cdd7ec20bcb
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-10-21 16:00:03 +0200

    Aggiunto campo calcolo generale prezzo senza sconto e aggiunto sconto minori

--- js/script.js
+++ js/script.js
@@ -14,15 +14,20 @@ const elemento = document.getElementById("mio_id");
 // Chiedere all utente quanti km deve percorrere 
 let quantiKm = parseInt(prompt("Quanti km devi percorrere?"));
 console.log(quantiKm);
+
 // Chiedere all utente la sua età
 let quantiAnni = parseInt(prompt("Quanti anni hai?"));
+
 // Stabilire prezzo al km 
 const prezzoAlKm = 0.21;
 console.log("Il prezzo al km è di:",(prezzoAlKm), "€");
 
-
-
+// Calcola il prezzo in base ai km inseriti dall utente ma senza sconto 
+let prezzoBase = quantiKm * prezzoAlKm;
+console.log(prezzoBase);
 // inserire sconto per i minorenni
+let scontoMinori = ( prezzoBase * 20 ) /100;
+console.log(scontoMinori);
 
 // inserire sconto per gli over 65
 
